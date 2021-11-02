67a09ac7cd3a7ee9833f8c190847506f260303dd
h598999 <89342804+h598999@users.noreply.github.com>
2021-11-02 14:42:51 +0100

    Update SkrivBlogg.java

--- src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab11/oppgave4/SkrivBlogg.java
@@ -3,13 +3,22 @@ package no.hvl.dat100.jplab11.oppgave4;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-import no.hvl.dat100.jplab11.common.TODO;
-import no.hvl.dat100.jplab11.oppgave3.*;
+import no.hvl.dat100.jplab11.oppgave3.Blogg;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		boolean a = true;
+		try {
+ PrintWriter skriver = new PrintWriter(filnavn);
+ skriver.print(samling.toString());
+ skriver.close();}
+ 
+ catch(FileNotFoundException e) {
+	  a = false;
+	 System.out.println("Fil ikkje funnet");
+ }
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+return a;	
+}
 }
