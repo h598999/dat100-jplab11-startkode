183d45c34df0c5ae706ac33b7b365c4d5389744f
h598999 <89342804+h598999@users.noreply.github.com>
2021-11-02 14:41:34 +0100

    Update Bilde.java

--- src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
+++ src/no/hvl/dat100/jplab11/oppgave2/Bilde.java
@@ -4,28 +4,31 @@ import no.hvl.dat100.jplab11.common.TODO;
 
 public class Bilde extends Tekst {
 
-	// TODO - objekt variable
+	private String url;
 	
 	public Bilde(int id, String bruker, String dato, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+		super(id, bruker, dato, tekst);
+		this.url = url;
 	}
 
 	public Bilde(int id, String bruker, String dato, int likes, String tekst, String url) {
-		throw new UnsupportedOperationException(TODO.constructor("Bilde"));
+	super(id, bruker, dato, likes, tekst);
+	this.url = url;
 	}
 	
 	public String getUrl() {
-		throw new UnsupportedOperationException(TODO.method());
+		return url;
 
 	}
 
-	public void setUrl(String url) {
-		throw new UnsupportedOperationException(TODO.method());
+	public void setUrl(String nyurl) {
+	url = nyurl;
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method ());
+		String str = super.toString().replace("TEKST", "BILDE") + url + "\n";
+		return str;
 
 	}
 
